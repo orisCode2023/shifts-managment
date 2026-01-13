5949bab15b7b53cb2941707adf917928c522a6d7
Ori <odmark2023@gmail.com>
2026-01-13 22:15:57 +0200

    create the routes of the users

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,39 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Post,
+  Patch,
+  Delete,
+} from '@nestjs/common';
+import { UsersService } from './users.service';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Get()
+  findAll() {
+    return this.usersService.findAll();
+  }
+  @Get(':id')
+  findOne(@Param('id') id: number) {
+    return this.usersService.findOne(id);
+  }
+  @Post()
+  createUser(@Body() user: { name: string; roll: string }) {
+    return this.usersService.createUser(user);
+  }
+  @Patch(':id')
+  updateUser(
+    @Param('id') id: string,
+    @Body() updatedUser: { name: string; roll: string },
+  ) {
+    return this.usersService.updateUser(+id, updatedUser);
+  }
+  @Delete(':id')
+  removeUser(@Param('id') id: number) {
+    return this.usersService.removeUser(+id);
+  }
+}
