c568e005f6159352adcae3d26faee1f33eb2be61
odmark2023@gmail.com <odmark2023@gmail.com>
2026-01-14 17:58:30 +0200

    adding connection to db

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,16 @@ import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { ShiftsModule } from './shifts/shifts.module';
 import { AssignmentsModule } from './assignments/assignments.module';
+import { SequelizeModule } from '@nestjs/sequelize';
+
 
 @Module({
-  imports: [AuthModule, UsersModule, ShiftsModule, AssignmentsModule],
+  imports: [AuthModule, UsersModule, ShiftsModule, AssignmentsModule, SequelizeModule.forRoot({
+     dialect: 'postgres',
+      uri: 'postgresql://neondb_owner:npg_M7JpHwtZXFc0@ep-holy-sky-ahwvwiig-pooler.c-3.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require',
+      autoLoadModels: true,
+      synchronize: true,
+    }),],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+import sequelize from 'sequelize';
+import { SequelizeModule } from '@nestjs/sequelize';
 
 @Module({
+  imports: [SequelizeModule.forFeature([
+    // Add your User model here
+  ])],
   controllers: [UsersController],
   providers: [UsersService],
   exports: [UsersService],
